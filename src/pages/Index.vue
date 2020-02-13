2c8f5c9b045bf8615d5a21e240b1b83df4036965
Helmoz <helmo.designer1@gmail.com>
2020-02-14 01:11:21 +0300

    Реализован компонент настроек крафта: выбор города, количество  очков фокуса и количество предметов

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,19 @@
 <template>
-  <q-page> </q-page>
+  <q-page><craft-settings /></q-page>
 </template>
 
 <script>
 import items from "../statics/items.json";
+import CraftSettings from "../components/craft-settings";
+import { useStore } from "../composition/use-store";
 
 export default {
   name: "PageIndex",
+  components: {
+    CraftSettings
+  },
   setup() {
+    const store = useStore();
     return {};
   }
 };
